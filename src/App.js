0f4973b1c2e1e30f982019b8d9e5cbb30cb1e176
chaoting <02115214@gm.scu.edu.tw>
2020-11-28 15:42:04 +0800

    2020.11.28 15:41 finish box UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react';
 import styled from '@emotion/styled'
+// import { ThemeProvider } from 'emotion-theming';書裡引入方法不能用了
+import { useTheme, ThemeProvider, withTheme } from '@emotion/react'
+
+import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
+import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
+import { ReactComponent as RainIcon } from './images/rain.svg';
+import { ReactComponent as RefreshIcon } from './images/refresh.svg';
+
+//定義主題配色
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
-  background-color: #ededed;
   height: 100%;
   display: flex;
   align-items: center;
@@ -13,20 +41,170 @@ const Container = styled.div`
 const WeatherCard= styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
   `;
+// 透過 props 取得傳進來的資料
+// props 會是 {theme: "dark", children: "台北市"}
+const Location = styled.div`
+  ${'' /* ${props => console.log(props)} */}
+  ${'' /* color: ${props => props.theme === 'dark' ? '#dadada' : '#212121'}; */}
+  color: ${({ theme }) => theme.titleColor};
+  font-size: 28px;
+  margin-bottom: 20px;
+`;
+  const Description = styled.div`
+  font-size: 16px;
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 30px;
+`;
   
-function App() {
+  const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+color: ${({ theme }) => theme.temperatureColor};
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textColor};
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
+`;
+
+// const Refresh = styled(RefreshIcon)`
+//   /* 在這裡寫入 CSS 樣式 */
+//   width: 15px;
+//   height: 15px;
+//   position: absolute;
+//   right: 15px;
+//   bottom: 15px;
+//   cursor: pointer;
+// `;
+
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: ${({ theme }) => theme.textColor};
+  svg {
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+  }
+`;
+
+const App = () => {
+  // const [currentTheme, setCurrentTheme] = useState('light');
+  const [currentTheme, setCurrentTheme] = useState('dark');
+
+   //定義會使用到的資料狀態
+   const [currentWeather, setCurrentWeather] = useState({
+    observationTime: '2020-12-12 22:10:00',
+    locationName: '臺北市',
+    description: '多雲時晴',
+    windSpeed: 3.6,
+    temperature: 32.1,
+    rainPossibility: 60,
+  });
+  async function getDataFromServer() {
+    const url = 'http://35.194.203.197/test.php';
+
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    });
+    const response = await fetch(request);
+    const data = await response.json();
+
+    console.log('data', data);
+  }
+
+  // componentDidMount，一掛載就GET會員資料表
+  useEffect(() => {
+    getDataFromServer();
+    console.log('一掛載就讀取資料表');
+  }, []);
+
+
   return (
+    // <ThemeProvider theme={theme.dark}>//寫死的版本
+    // <ThemeProvider theme={theme.currentTheme}>//不能寫成這樣會爛掉
+    <ThemeProvider theme={theme[currentTheme]}>
     <Container>
-    <WeatherCard>
-    <h1>Weather</h1>
-    </WeatherCard>
+      <WeatherCard>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>°C</Celsius>
+          </Temperature>
+          <DayCloudy/>
+        </CurrentWeather>
+        <AirFlow>
+        <AirFlowIcon/>
+        23 m/h
+        </AirFlow>
+        <Rain>
+        <RainIcon/>
+        48%
+        </Rain>
+        <Refresh>
+            最後觀測時間：上午 12:03 <RefreshIcon />
+          </Refresh>
+      </WeatherCard>
     </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
